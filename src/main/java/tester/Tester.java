3ee64f2ae1888c49243fdf765048e89036cdaef1
Lars-m <lam@cphbusiness.dk>
2020-08-26 17:55:41 +0200

    exercise

--- src/main/java/tester/Tester.java
+++ src/main/java/tester/Tester.java
@@ -22,37 +22,21 @@ public class Tester {
             em.persist(new Employee("jik666", "Tian", "Wonnegut", new BigDecimal(56567)));
             em.getTransaction().commit();
             
+            //Complete all these small tasks. Your will find the solution to all, but the last,
+            //In this document: https://en.wikibooks.org/wiki/Java_Persistence/JPQL#JPQL_supported_functions
+            
+            //1) Create a query to fetch all employees with a salary > 100000 and print out all the salaries
+            
+            //2) Create a query to fetch the employee with the id "klo999" and print out the firstname
+            
+            //3) Create a query to fetch the highest salary and print the value
 
-            Query query = em.createQuery("Select e FROM Employee e WHERE e.salary > 100000");
-            List<Employee> result = query.getResultList();
-            result.forEach(e -> {
-                System.out.println(e.getSalary());
-            });
-
-            query = em.createQuery("Select e FROM Employee e WHERE e.id = :id");
-            query.setParameter("id", "klo999");
-            Employee result2 = (Employee) query.getSingleResult();
-            System.out.println(result2.getFirstName());
-
-            // Query for a single data element.
-            query = em.createQuery("Select MAX(e.salary) FROM Employee e");
-            BigDecimal result3 = (BigDecimal) query.getSingleResult();
-            System.out.println(result3);
-
-            query = em.createQuery("Select e.firstName FROM Employee e");
-            List<String> result4 = query.getResultList();
-            result4.forEach(fn -> {
-                System.out.println(fn);
-            });
-
-            query = em.createQuery("SELECT COUNT(e) FROM Employee e");
-            Long employeeCount = (long) query.getSingleResult();
-            System.out.println("Total: " + employeeCount);
-
-            query = em.createQuery("select e from Employee e where e.salary = (Select MAX(e2.salary) FROM Employee e2)");
-            Employee e = (Employee) query.getSingleResult();
-            System.out.println(e);
-
+            //4) Create a query to fetch the firstName of all Employees and print the names
+           
+            //5 Create a query to calculate the number of employees and print the number
+            
+            //6 Create a query to fetch the Employee with the higest salary and print all his details
+            
         } finally {
             em.close();
             emf.close();
